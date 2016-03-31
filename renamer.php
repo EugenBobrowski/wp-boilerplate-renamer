98ea4155deb39466f39fdf199ed8a86fb18de0f0
Eugen Bobrowski <ecologistkai@gmail.com>
2016-03-31 13:39:59 +0300

    Style the rename page with BS style elenets.

--- renamer.php
+++ renamer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm. Recursively
  * User: eugen
@@ -7,8 +8,8 @@
  *
  * http://site.loc/wp-content/themes/twentyfifteen/renamer.php
  */
-
-class Renamer {
+class Renamer
+{
     /**
      * @var string
      */
@@ -26,7 +27,8 @@ class Renamer {
      */
     public $replace_array;
 
-    public function __construct() {
+    public function __construct()
+    {
 
     }
 
@@ -35,7 +37,8 @@ class Renamer {
         $this->ignore[] = $file;
     }
 
-    public function generate_replace_array() {
+    public function generate_replace_array()
+    {
         $replace_array = array(
             $this->search => $this->replace,
         );
@@ -43,64 +46,52 @@ class Renamer {
         $replace_exploded = explode(' ', $this->replace);
 
         //    'twentyfifteen' => 'twentysomething',
-        $replace_array[strtolower(implode('',$search_exploded))] = strtolower(implode('',$replace_exploded));
+        $replace_array[strtolower(implode('', $search_exploded))] = strtolower(implode('', $replace_exploded));
         //    'twenty-fifteen' => 'twenty-something',
-        $replace_array[strtolower(implode('-',$search_exploded))] = strtolower(implode('-',$replace_exploded));
+        $replace_array[strtolower(implode('-', $search_exploded))] = strtolower(implode('-', $replace_exploded));
         //    'twenty_fifteen' => 'twenty_something',
-        $replace_array[strtolower(implode('_',$search_exploded))] = strtolower(implode('_',$replace_exploded));
+        $replace_array[strtolower(implode('_', $search_exploded))] = strtolower(implode('_', $replace_exploded));
         //    'TWENTY_FIFTEEN' => 'TWENTY_SOMETHING',
-        $replace_array[strtoupper(implode('_',$search_exploded))] = strtoupper(implode('_',$replace_exploded));
+        $replace_array[strtoupper(implode('_', $search_exploded))] = strtoupper(implode('_', $replace_exploded));
 
         $this->replace_array = $replace_array;
 
     }
 
     /**
-     * @param $content string
-     * @return string
-     */
-    public function str_replace_custom ($content) {
-
-        foreach ($this->replace_array as $search=>$replace) {
-            $content = str_replace($search, $replace, $content);
-        }
-
-        return $content;
-    }
-
-    /**
      * @param $path
      */
-    public function scanTheDir($path) {
+    public function scanTheDir($path)
+    {
         echo '<ul>';
         $items = scandir($path);
         foreach ($items as $item) {
-            $path_item = $path.'/'.$item;
+            $path_item = $path . '/' . $item;
             if ($item == '.' || $item == '..') {
 //            echo $item;
-            } elseif (realpath($path_item) == __FILE__ ) {
+            } elseif (realpath($path_item) == __FILE__) {
                 echo '<li>';
-                echo '<em>THIS FILE '.__FILE__.'</em>';
+                echo '<em>THIS FILE ' . __FILE__ . '</em>';
                 echo '</li>';
-            }  elseif (in_array($path_item, $this->ignore)) {
+            } elseif (in_array($path_item, $this->ignore)) {
                 echo '<li>';
                 echo '<em>' . $item . '</em>';
                 echo ' [IGNORED] ';
                 echo '</li>';
             } elseif (is_dir($path_item)) {
                 echo '<li>';
-                echo '<strong>'.$item.'</strong>';
+                echo '<strong>' . $item . '</strong>';
                 $path_item = $this->renamefiles($path, $item);
                 $this->scanTheDir($path_item);
                 echo '</li>';
             } else {
                 echo '<li>';
-                echo '<em>'.$item.'</em>';
+                echo '<em>' . $item . '</em>';
                 $path_item = $this->renamefiles($path, $item);
                 $content = file_get_contents($path_item);
                 $new_content = $this->str_replace_custom($content);
                 if ($new_content != $content && file_put_contents($path_item, $new_content)) {
-                    echo ' [REPLACED] ';
+                    echo ' <span class="label label-warning">Replaced</span> ';
                 }
                 echo '</li>';
             }
@@ -113,18 +104,33 @@ class Renamer {
      * @param $item
      * @return string
      */
-    public function renamefiles($path, $item){
+    public function renamefiles($path, $item)
+    {
         $new_name = $this->str_replace_custom($item);
-        if ($new_name != $item && rename($path.'/'.$item, $path.'/'.$new_name)) {
-            echo ' >> '.$new_name;
-            return $path.'/'.$new_name;
+        if ($new_name != $item && rename($path . '/' . $item, $path . '/' . $new_name)) {
+            echo ' <span class="glyphicon glyphicon-menu-right"></span>  ' . $new_name;
+            echo ' <span class="label label-danger">Renamed</span> ';
+            return $path . '/' . $new_name;
         } else {
-            return $path.'/'.$item;
+            return $path . '/' . $item;
         }
     }
 
-}
+    /**
+     * @param $content string
+     * @return string
+     */
+    public function str_replace_custom($content)
+    {
 
+        foreach ($this->replace_array as $search => $replace) {
+            $content = str_replace($search, $replace, $content);
+        }
+
+        return $content;
+    }
+
+}
 
 
 ?>
@@ -152,38 +158,40 @@ class Renamer {
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
-<body>
-<h1>Boilerplate Renamer</h1>
+<body style="background: #473E48; padding: 30px 0;">
+<div class="container" style="background: #ffffff; border-radius: 2px; padding-top: 15px; padding-bottom: 15px; ">
+    <div class="page-header">
+        <h1>Boilerplate Renamer</h1>
+    </div>
+    <?php
 
-<?php
+    $renameObject = new Renamer();
 
-$renameObject = new Renamer();
-//
-//$renameObject->replace_array = array(
-//    'Twenty Fifteen' => 'Twenty Something',
-//    'twentyfifteen' => 'twentysomething',
-//    'twenty-fifteen' => 'twenty-something',
-//    'twenty_fifteen' => 'twenty_something',
-//    'TWENTY_FIFTEEN' => 'TWENTY_SOMETHING',
-//);
+    /**
+     * You can generate this with next three lines. Or add another search-replace items to array to the beginning or to the end, but after generation of replace array.
+     */
 
-/**
- * You can generate this with next three lines. Or add another search-replace items to array to the beginning or to the end, but after generation of replace array.
- */
+    $renameObject->search = 'Twenty Fifteen';
+    $renameObject->replace = 'Twenty Something';
+    $renameObject->generate_replace_array();
+    $renameObject->replace_array = array_merge($renameObject->replace_array, array(
+        $renameObject->search . ':' => 'Plugin Name:', //Fix plugin description replacement
+        'Your Name or Your Company' => 'Your Name or Your Company',
+        'http://example.com/' => 'http://example.com/',
+        'WordPress Plugin Boilerplate' => 'WordPress Plugin Boilerplate'
+    ));
+    $renameObject->addIgnore('.//.git');
 
-$renameObject->search = 'Twenty Fifteen';
-$renameObject->replace = 'Twenty Something';
-$renameObject->generate_replace_array();
-$renameObject->addIgnore('.//.git');
+    /**
+     * Run the renaming process
+     */
 
-/**
- * Run the renaming process
- */
-
-$renameObject->scanTheDir('./');
+    $renameObject->scanTheDir('./');
 
 
-?>
+    ?>
+</div>
+
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
